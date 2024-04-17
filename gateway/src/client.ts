7832807817f88766110e907fff8dfaf0ed5551e9
minhhoanq <mh.tranminhhoang@gmail.com>
2024-04-18 03:18:22 +0700

    grpc from gateway to product_service

--- gateway/src/client.ts
+++ gateway/src/client.ts
@@ -1,21 +1,39 @@
-const PROTO_PATH = "./src/proto/user.proto";
+const PROTO_PATH_USER = "./src/proto/user.proto";
+const PROTO_PATH_PRODUCT = "./src/proto/product.proto";
 
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
 
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+const packageDefinitionUser = protoLoader.loadSync(PROTO_PATH_USER, {
     keepCase: true,
     longs: String,
     enums: String,
     oneofs: true,
 });
 
-const UserService: any =
-    grpc.loadPackageDefinition(packageDefinition).UserService;
+const packageDefinitionProduct = protoLoader.loadSync(PROTO_PATH_PRODUCT, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    oneofs: true,
+});
 
-const client = new UserService(
+const UserService: any = grpc.loadPackageDefinition(
+    packageDefinitionUser
+).UserService;
+
+const ProductService: any = grpc.loadPackageDefinition(
+    packageDefinitionProduct
+).ProductService;
+
+const clientUser = new UserService(
     "127.0.0.1:30043",
     grpc.credentials.createInsecure()
 );
 
-export default client;
+const clientProduct = new ProductService(
+    "127.0.0.1:30044",
+    grpc.credentials.createInsecure()
+);
+
+export { clientUser, clientProduct };
